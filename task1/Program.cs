7a528087a7c081d462261959ffe8ad6c5de42955
nyshashina <nyshashina@gmail.com>
2023-02-04 17:54:51 +0300

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите порядковый номер дня недели");
+int day = int.Parse(Console.ReadLine() ?? "0");
+
+if(day > 0 && day < 8){
+    if (day == 6 || day == 7){
+        Console.WriteLine("Это выходной.");
+    }
+    else {
+        Console.WriteLine("Это рабочий день.");
+    }
+}
+else {
+    Console.WriteLine("В неделе 7 дней.");
+}
